f30955c0ec98659e85245678c1a913bb578b855a
advdylan <immortalizetimes@gmail.com>
2023-11-21 16:44:44 +0100

    modele

--- Selfcare/patients/models.py
+++ Selfcare/patients/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Patients(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_lenghts=30)
