d12b483ae1b90126af514c4910e4a555b6336fc0
advdylan <immortalizetimes@gmail.com>
2023-11-22 16:41:58 +0100

    gicior idzie z modelami

--- Selfcare/patients/admin.py
+++ Selfcare/patients/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from.models import Patient, Doctor, Meetings
 
-# Register your models here.
+class MeetingsAdmin(admin.ModelAdmin):
+    list_display = ("meeting_place", "duration", "doctor","patient")
+
+class PatientsAdmin(admin.ModelAdmin):
+    filter_horizontal = ("meetings",)
+
+admin.site.register(Doctors)
+admin.site.register(Meetings, MeetingsAdmin)
+
+
--- Selfcare/patients/models.py
+++ Selfcare/patients/models.py
@@ -1,24 +1,48 @@
 from django.db import models
+from django.db.models import F
+from datetime import timedelta
 
 # Create your models here.
 
-class Doctors(models.Model):
+class Doctor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     address = models.CharField(max_length=100)
     phone_number = models.IntegerField()
-    proteges = models.ForeignKey('Patients', on_delete=models.CASCADE, related_name="Patients")
+    proteges = models.ManyToManyField('Patient', related_name="Patients")
 
-class Patients(models.Model):
+class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     address = models.CharField(max_length=100)
     phone_number = models.IntegerField()
-    curator = models.ForeignKey(Doctors, on_delete=models.CASCADE, related_name="Doctor")
+    curator = models.ManyToManyField(Doctor, related_name="Doctor")
 
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}, address: {self.address}, phone_number: {self.phone_number}"
 
+class Meetings(models.Model):
+    MEETING_TYPE = (
+        ('Office', 'Office'),
+        ('Remote', ' Remote'),
+    )
+
+    meeting_place = models.CharField(max_length=100, null=True, choices=MEETING_TYPE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    duration = models.DurationField(null=True)
+    doctor = models.ForeignKey(Doctor, null=True, on_delete=models.SET_NULL)
+    patient = models.ForeignKey(Patient, null=True, on_delete=models.SET_NULL)
 
 
+    def save(self,*args, **kwargs):
+        if self.end_time <= self.start_time:
+            raise ValueError("End time must be after the start time")
+        
+        self.duration = self.end_time - self.start_time
+        super(Meetings,self).save(*args,**kwargs)
+
+    
+
+    
