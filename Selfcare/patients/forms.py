5d98b1fac1a7990f7bfdc7ebcd336cdca9cdbaf4
advdylan <immortalizetimes@gmail.com>
2023-12-03 08:43:12 +0100

    123

--- Selfcare/patients/forms.py
+++ Selfcare/patients/forms.py
@@ -6,8 +6,8 @@ from .models import Doctor,Patient,Meetings
 
 #calendar custom widget
 
-class DateInput(forms.DateInput):
-    input_type = 'date'
+class DateTimeInput(forms.DateTimeInput):
+    input_type = 'datetime-local'
 
 
 class NewPatient(forms.ModelForm):
@@ -49,7 +49,7 @@ class NewMeeting(forms.ModelForm):
 
         widgets = {
             'meeting_place': forms.Select(attrs={'class': 'form-select'}),
-            'start_time': DateInput(attrs={'class': 'form-select'}),
+            'start_time': forms.DateTimeInput(attrs={'class': 'form-select'}),
             'doctor': forms.Select(attrs={'class': 'form-select'}),
             'patient': forms.Select(attrs={'class': 'form-select'})
         }
