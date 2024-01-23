7dccfe3ecfbed34652022607ca343f99805e23b0
advdylan <immortalizetimes@gmail.com>
2024-01-23 19:03:25 +0100

    media dir

--- Selfcare/Selfcare/settings.py
+++ Selfcare/Selfcare/settings.py
@@ -16,6 +16,7 @@ from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_DIR = BASE_DIR / 'media'
 
 
 # Quick-start development settings - unsuitable for production
